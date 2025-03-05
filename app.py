2427a5daabaebdc2790df2681df2cb5b76895eb3
JeisonMello <jeisonmello@icloud.com>
2025-03-05 13:52:05 +1100

    Update app.py

--- app.py
+++ app.py
@@ -101,11 +101,18 @@ if ticker_input:
         dividendos = stock.dividends.resample("Y").sum().tail(10)
         anos = dividendos.index.year
         
+        # Calcular preço médio anual para obter o Dividend Yield (%)
+        preco_medio_anual = stock.history(period="10y")["Close"].resample("Y").mean()
+        preco_medio_anual.index = preco_medio_anual.index.year
+        preco_medio_anual = preco_medio_anual.reindex(anos, fill_value=1)
+        dividend_yield = (dividendos / preco_medio_anual) * 100  
+        dividend_yield = dividend_yield.replace([float("inf"), -float("inf")], 0).fillna(0)
+        
         fig_divid = go.Figure()
         fig_divid.add_trace(go.Bar(
             x=anos,
-            y=dividendos,
-            text=dividendos.apply(lambda x: f"{x:.2f}"),
+            y=dividend_yield,
+            text=dividend_yield.apply(lambda x: f"{x:.2f}%"),
             textposition='outside',
             marker=dict(color="#ad986e", opacity=0.8, line=dict(color="rgba(0, 0, 0, 0.3)", width=1)),
         ))
@@ -114,7 +121,7 @@ if ticker_input:
             template="plotly_dark",
             title=f"Dividend Yield - Últimos 10 Anos ({ticker})",
             xaxis_title="Ano",
-            yaxis_title="Dividendos (R$)",
+            yaxis_title="Yield (%)",
             margin=dict(l=40, r=40, t=40, b=40),
             font=dict(color="white")
         )
@@ -124,11 +131,11 @@ if ticker_input:
         # 📌 Estatísticas de Dividendos
         st.subheader(f"📊 Estatísticas de Dividendos - {ticker}")
         ultimo_dividendo = dividendos.iloc[-1] if not dividendos.empty else 0
-        media_10_anos = dividendos.mean()
+        media_10_anos = dividend_yield.mean()
         anos_sem_dividendo = dividendos[dividendos == 0].index.tolist()
 
-        st.write(f"🔹 **Último dividendo pago:** {ultimo_dividendo:.2f}")
-        st.write(f"🔹 **Média dos últimos 10 anos:** {media_10_anos:.2f}")
+        st.write(f"🔹 **Último dividend yield:** {ultimo_dividendo:.2f}%")
+        st.write(f"🔹 **Média dos últimos 10 anos:** {media_10_anos:.2f}%")
 
         if anos_sem_dividendo:
             st.write(f"❌ **Anos sem pagamento de dividendos:** {', '.join(map(str, anos_sem_dividendo))}")
