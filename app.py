f0d95c0940bfb486d6d4c8e3adc717789c0b228f
JeisonMello <jeisonmello@icloud.com>
2025-03-10 16:34:59 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 import streamlit as st
-import sys
-import os
-
-# Adiciona o diretório raiz ao caminho do Python
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
-try:
-    from auth.login import login  # Importação padrão
-except ModuleNotFoundError:
-    st.error("Erro ao importar auth.login. Verifique o caminho e o nome dos arquivos.")
+from auth.login import login  # Agora a importação deve funcionar corretamente!
 
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
 
 if not st.session_state['logged_in']:
-    login()  # Agora chamamos a função corretamente
+    login()  # Agora chamamos a função login corretamente
 else:
     st.sidebar.title("Navegação")
     pagina = st.sidebar.selectbox("Escolha uma página", ["Dashboard", "Histórico"])
