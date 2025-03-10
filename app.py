e4c7d608e2f600ff972d89a962e772fa4ae1a5a5
JeisonMello <jeisonmello@icloud.com>
2025-03-10 16:24:35 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
+import sys
+import os
+
+# Garantir que o diretório base seja reconhecido
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from auth.login import login  # Importando diretamente a função login
 
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
 
 if not st.session_state['logged_in']:
-    login()  # Agora chamamos a função diretamente
+    login()  # Agora chamamos a função corretamente
 else:
     st.sidebar.title("Navegação")
     pagina = st.sidebar.selectbox("Escolha uma página", ["Dashboard", "Histórico"])
