b60b3d0dd5fd3cb7409005589b905cf04991ad00
JeisonMello <jeisonmello@icloud.com>
2025-03-10 16:20:50 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from auth.login import login  # Importando a função login corretamente
+import auth.login as login  # Importa o módulo ao invés da função diretamente
 
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
 
 if not st.session_state['logged_in']:
-    login()  # Chamando a função login corretamente
+    login.login()  # Agora chama login() dentro do módulo auth.login
 else:
     st.sidebar.title("Navegação")
     pagina = st.sidebar.selectbox("Escolha uma página", ["Dashboard", "Histórico"])
