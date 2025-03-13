ad2dcaefb5de567dc1dc05b8befbdd21a2926979
JeisonMello <jeisonmello@icloud.com>
2025-03-13 17:39:17 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from admin.dashboard import admin_dashboard
-from admin.dashboard_stocks import dashboard_stocks
 from auth.login import login
+from admin.dashboard import admin_dashboard
 
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
@@ -10,11 +9,7 @@ if not st.session_state['logged_in']:
     login()
 else:
     st.sidebar.title("Navegação")
-    pagina = st.sidebar.selectbox("Escolha uma página", ["Dashboard", "Histórico", "Admin"])
+    pagina = st.sidebar.selectbox("Escolha uma página", ["Admin"])
 
-    if pagina == "Dashboard":
-        dashboard_stocks()  # Chama o Dashboard correto para ações
-    elif pagina == "Histórico":
-        st.write("Aqui vai o código do Histórico")
-    elif pagina == "Admin":
+    if pagina == "Admin":
         admin_dashboard()
