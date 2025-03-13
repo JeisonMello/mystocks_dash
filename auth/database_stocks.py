f3c1ab241de711a613cd656f68531f57d779bebf
JeisonMello <jeisonmello@icloud.com>
2025-03-13 17:58:19 +1100

    Update database_stocks.py

--- auth/database_stocks.py
+++ auth/database_stocks.py
@@ -25,35 +25,15 @@ def create_stocks_table():
 create_stocks_table()
 
 def add_stock(papel, nome, preco, custava, yield_val, preco_teto, setor, estrategia, obs):
-    """Adiciona uma nova ação ou atualiza uma já existente no banco de dados."""
+    """Adiciona uma ação ao banco de dados."""
     conn = sqlite3.connect("stocks.db")
     cursor = conn.cursor()
-
-    try:
-        cursor.execute("SELECT * FROM stocks WHERE papel = ?", (papel,))
-        existing_stock = cursor.fetchone()
-
-        if existing_stock:
-            cursor.execute('''
-                UPDATE stocks 
-                SET nome = ?, preco = ?, custava = ?, yield = ?, preco_teto = ?, setor = ?, estrategia = ?, obs = ?
-                WHERE papel = ?
-            ''', (nome, preco, custava, yield_val, preco_teto, setor, estrategia, obs, papel))
-            msg = f"Ação {papel} já existia e foi atualizada com sucesso!"
-        else:
-            cursor.execute('''
-                INSERT INTO stocks (papel, nome, preco, custava, yield, preco_teto, setor, estrategia, obs)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-            ''', (papel, nome, preco, custava, yield_val, preco_teto, setor, estrategia, obs))
-            msg = f"Ação {papel} adicionada com sucesso!"
-
-        conn.commit()
-    except sqlite3.Error as e:
-        msg = f"Erro ao adicionar ou atualizar ação {papel}: {e}"
-    finally:
-        conn.close()
-    
-    return msg
+    cursor.execute('''
+        INSERT INTO stocks (papel, nome, preco, custava, yield, preco_teto, setor, estrategia, obs)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (papel, nome, preco, custava, yield_val, preco_teto, setor, estrategia, obs))
+    conn.commit()
+    conn.close()
 
 def get_stocks():
     """Retorna todas as ações cadastradas."""
@@ -65,25 +45,9 @@ def get_stocks():
     return stocks
 
 def delete_stock(papel):
-    """Remove uma ação do banco de dados pelo código do papel, se existir."""
+    """Remove uma ação do banco de dados pelo código do papel."""
     conn = sqlite3.connect("stocks.db")
     cursor = conn.cursor()
-
-    try:
-        cursor.execute("SELECT * FROM stocks WHERE papel = ?", (papel,))
-        stock = cursor.fetchone()
-
-        if stock:
-            cursor.execute("DELETE FROM stocks WHERE papel = ?", (papel,))
-            conn.commit()
-            msg = f"Ação {papel} removida com sucesso!"
-        else:
-            msg = f"Ação {papel} não encontrada no banco de dados."
-
-    except sqlite3.Error as e:
-        msg = f"Erro ao excluir ação {papel}: {e}"
-
-    finally:
-        conn.close()
-    
-    return msg
+    cursor.execute("DELETE FROM stocks WHERE papel = ?", (papel,))
+    conn.commit()
+    conn.close()
