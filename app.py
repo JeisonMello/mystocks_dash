95d535c16cd6bd18dd90c84b388f7be60d206ad1
JeisonMello <jeisonmello@icloud.com>
2025-03-13 17:23:06 +1100

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
+from auth.login import login
 from admin.dashboard import admin_dashboard
-from admin.dashboard_stocks import dashboard_stocks
-from auth.login import login  # Certifique-se de que login.py contém uma função chamada login()
+from admin.dashboard_stocks import dashboard_stocks  # Agora temos os dois painéis
 
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
 
 if not st.session_state['logged_in']:
-    login()  # Certifique-se de que login() é uma função e não um módulo
+    login()
 else:
     st.sidebar.title("Navegação")
     pagina = st.sidebar.selectbox("Escolha uma página", ["Dashboard", "Histórico", "Admin"])
